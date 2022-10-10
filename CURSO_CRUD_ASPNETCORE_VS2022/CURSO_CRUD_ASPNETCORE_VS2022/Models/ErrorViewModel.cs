58705679e8f7a3d3aa689bd41274c36fd6ac4192
Diii Huete <diego1190@hotmail.es>
2022-10-09 20:23:55 -0600

    Mi proyecto Vacio

---
+++ CURSO_CRUD_ASPNETCORE_VS2022/CURSO_CRUD_ASPNETCORE_VS2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CURSO_CRUD_ASPNETCORE_VS2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
